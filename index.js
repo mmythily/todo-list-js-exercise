aac857d00f3195d692bbc937ab7fce09654ae025
mmythily <mythily.mudunuru@gmail.com>
2019-06-26 20:34:16 -0400

    add two functions - one prints the completion, another changes to complete

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ function newTask(title, description) {
   return task;
 }
 
+//print out task's completion
+function logTaskState(task) {
+  console.log(`${task.title} has ${task.complete ? " " : "not"} been completed`);
+}
+
+//mark called task as complete 
+function completeTask(task) {
+  task.complete = true;
+}
+
 //DRIVER CODE
 
 const task1 = newTask('Gotta do all the OOP reading', 'This is gonna take hella time');
 const task2 = newTask('Grocery shopping', 'so you can make food at home');
 const tasks = [task1,task2]
 
+logTaskState(task1);
+completeTask(task2)
 console.log(tasks);
